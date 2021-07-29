80a3e5c21e30c8b91a96a27c24ab4f17a47f98f1
DiscreteTom <discrete_tom@outlook.com>
2021-07-29 18:33:52 +0100

    fix config pin num

--- config.go
+++ config.go
@@ -3,19 +3,19 @@ package main
 type PinNum uint8
 
 const (
-	// physical pins of the motors
-	PWM_A_PIN  PinNum = 12 // BCM: 18
-	A_IN_1_PIN PinNum = 15 // BCM: 22
-	A_IN_2_PIN PinNum = 13 // BCM: 27
-	PWM_B_PIN  PinNum = 16 // BCM: 23
-	B_IN_1_PIN PinNum = 22 // BCM: 25
-	B_IN_2_PIN PinNum = 18 // BCM: 24
+	// BCM pins of the motors
+	PWM_A_PIN  PinNum = 18
+	A_IN_1_PIN PinNum = 22
+	A_IN_2_PIN PinNum = 27
+	PWM_B_PIN  PinNum = 23
+	B_IN_1_PIN PinNum = 25
+	B_IN_2_PIN PinNum = 24
 
-	// physical pins of PS2 controller
-	PS2_DAT_PIN PinNum = 29 // BCM: 5
-	PS2_CMD_PIN PinNum = 31 // BCM: 6
-	PS2_SEL_PIN PinNum = 33 // BCM: 13
-	PS2_CLK_PIN PinNum = 35 // BCM: 19
+	// BCM pins of PS2 controller
+	PS2_DAT_PIN PinNum = 5
+	PS2_CMD_PIN PinNum = 6
+	PS2_SEL_PIN PinNum = 13
+	PS2_CLK_PIN PinNum = 19
 
 	// PS2 controller clock cycle
 	PS2_CLK_CYCLE uint8 = 20 // us
